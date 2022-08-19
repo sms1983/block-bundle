f6e09d0cfca65fc849a0b1a6adfd40d132fc0307
Yan Bein <rbeinyan@gmail.com>
2022-08-19 10:57:19 -0300

    Fix

--- src/Doctrine/Phpcr/ContainerBlock.php
+++ src/Doctrine/Phpcr/ContainerBlock.php
@@ -34,7 +34,7 @@ class ContainerBlock extends AbstractBlock
     /**
      * {@inheritdoc}
      */
-    public function getType()
+    public function getType(): ?string
     {
         return 'cmf.block.container';
     }
@@ -89,7 +89,7 @@ class ContainerBlock extends AbstractBlock
      *
      * @return bool
      */
-    public function addChildren(BlockInterface $children)
+    public function addChildren(\Sonata\BlockBundle\Model\BlockInterface $children): void
     {
         return $this->addChild($children);
     }
@@ -111,7 +111,7 @@ class ContainerBlock extends AbstractBlock
     /**
      * {@inheritdoc}
      */
-    public function hasChildren()
+    public function hasChildren(): bool
     {
         return count($this->children) > 0;
     }
